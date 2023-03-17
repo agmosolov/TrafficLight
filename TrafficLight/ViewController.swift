cacfd8edecba9d935f2906d6746e8e187afc4a76
Aleksander Mosolov <agmosolov@icloud.com>
2023-03-17 22:15:46 +0300

    Set UI and Links with class

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pressedButton(_ sender: UIButton) {
+    }
 }
 
